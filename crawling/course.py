1a2cc32c8084e25d1329c6b8716de689b2b18ffc
hyunmin <choihm9903@naver.com>
2022-01-13 15:41:20 +0900

    Update crawling

--- crawling/course.py
+++ crawling/course.py
@@ -1,54 +1,26 @@
-import requests
+from time import sleep
+
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-def course():
-    URL = 'https://ecampus.smu.ac.kr/'
+def login(id, password):
+    URL = "https://ecampus.smu.ac.kr/login.php"
 
-def main():
-    URL = 'https://ecampus.smu.ac.kr/report/ubcompletion/user_progress.php?id=63139'
-    html = requests.get(URL)
-    print(html.text)
-    print(html.url)
-    # soup = BeautifulSoup(html.text, 'html.parser')
-    # print(soup)
-
-    # response = requests.post(URL).text
-    # print(response)
-    # soup = BeautifulSoup(response, 'lxml', from_encoding='utf-8')
-
-    urls = []
-    data = []
-
-    # print(soup)
-    # for href in soup.find("div", class_='result-list').find_all("dt"):
-    #     urls.append(BASIC+href.find("a")["href"])
-    #
-    #
-    #
-    # for url in urls:
-    #     soup = BeautifulSoup(urlopen(url), 'html.parser')
-    #     d = []
-    #
-    #     try:
-    #         for content in soup.find("div", class_='file-meta-table-pc').find_all("tr"):
-    #             td = (content.find('td').text).replace(u'\xa0', u'').replace(u'\t', u'').replace(u'\n', u'')
-    #             d.append(td)
-    #
-    #         if len(d) == 14:
-    #             d.append(url)
-    #         else:
-    #             url = d[10]
-    #             d[10:] = d[11:]
-    #             d.append(url)
-    #         data.append(d)
-    #         print(len(data))
-    #     except:
-    #         pass
-    #
-    # df = pd.DataFrame(data)
-    # print(df)
-    # df.to_csv('public-data.csv', index=False, encoding='utf-8-sig')
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(URL)
+    driver.find_element(By.ID, 'input-username').send_keys(id)
+    driver.find_element(By.ID, 'input-password').send_keys(password)
+    driver.find_element(By.NAME, 'loginbutton').click()
+    return driver.page_source
 
 if __name__ == '__main__':
-    main()
+    url = login("201911019", "1q2w3e4r!!!")
+    soup = BeautifulSoup(url, 'html.parser')
+    courses = soup.find_all("div", class_="course-title")
+
+    print(" 강좌 개수: "+str(len(courses))+"\n\n 수업 목록")
+    for course in courses:
+        print(" 강좌: "+course.find("h3").text)
+        print(" 교수: "+course.find("p").text)
