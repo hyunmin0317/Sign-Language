3d41bf8ae1a379e67c7ebeadf08d427b79d92ab3
hyunmin <choihm9903@naver.com>
2022-03-29 21:34:28 +0900

    fix: path error

--- detect/viewsdl.py
+++ detect/viewsdl.py
@@ -57,81 +57,81 @@ class VideoCamera(object):
     def get_frame(self):
         ret, img = cap.read()
 
-        # img = cv2.flip(img, 1)
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # result = hands.process(img)
-        # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        # 
-        # 
-        # if result.multi_hand_landmarks is not None:
-        #     for res in result.multi_hand_landmarks:
-        #         joint = np.zeros((21, 4))
-        #         for j, lm in enumerate(res.landmark):
-        #             joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
-        # 
-        #         # Compute angles between joints
-        #         v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
-        #         v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
-        #         v = v2 - v1 # [20, 3]
-        #         # Normalize v
-        #         v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
-        # 
-        #         # Get angle using arcos of dot product
-        #         angle = np.arccos(np.einsum('nt,nt->n',
-        #             v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
-        #             v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
-        # 
-        #         angle = np.degrees(angle) # Convert radian to degree
-        # 
-        #         d = np.concatenate([joint.flatten(), angle])
-        # 
-        #         seq.append(d)
-        # 
-        #         mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
-        # 
-        #         if len(seq) < seq_length:
-        #             continue
-        # 
-        #         input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
-        #         # 인퍼런스 한 결과를 뽑아낸다
-        #         y_pred = model.predict(input_data).squeeze()
-        #         # 어떠한 인덱스 인지 뽑아낸다
-        #         i_pred = int(np.argmax(y_pred))
-        #         conf = y_pred[i_pred]
-        #         # confidence 가 90%이하이면 액션을 취하지 않았다 판단
-        #         if conf < 0.9:
-        #             continue
-        # 
-        #         action = actions[i_pred]
-        #         action_seq.append(action)
-        # 
-        #         if len(action_seq) < 7:
-        #             continue
-        # 
-        #         # 마지막 3번 반복되었을 때 진짜로 판단
-        #         #this_action = '?'
-        #         if action_seq[-1] == action_seq[-2] == action_seq[-3]:
-        #             #this_action = action
-        # 
-        #             if action == '삭제':
-        #                 word.clear()
-        #             else:
-        #                 word.append(action)
-        #         img = Image.fromarray(img)
-        #         draw = ImageDraw.Draw(img)
-        #         
-        #         draw.text(xy=(int(res.landmark[0].x*640), int(res.landmark[0].y*480 + 20)), text=action, font = font, fill=(255,255,255))
-        #         img = np.array(img)
-        # content = ''
-        # for i in word:
-        #     if i in content:
-        #         pass
-        #     else:
-        #         content += i
-        #         content += " "     
-        # img = Image.fromarray(img)
-        # draw = ImageDraw.Draw(img)
-        # draw.text(xy=(10,30), text=content, font = font, fill=(255,255,255))
+        img = cv2.flip(img, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = hands.process(img)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+
+        if result.multi_hand_landmarks is not None:
+            for res in result.multi_hand_landmarks:
+                joint = np.zeros((21, 4))
+                for j, lm in enumerate(res.landmark):
+                    joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
+
+                # Compute angles between joints
+                v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
+                v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
+                v = v2 - v1 # [20, 3]
+                # Normalize v
+                v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+                # Get angle using arcos of dot product
+                angle = np.arccos(np.einsum('nt,nt->n',
+                    v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:],
+                    v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+
+                angle = np.degrees(angle) # Convert radian to degree
+
+                d = np.concatenate([joint.flatten(), angle])
+
+                seq.append(d)
+
+                mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+
+                if len(seq) < seq_length:
+                    continue
+
+                input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
+                # 인퍼런스 한 결과를 뽑아낸다
+                y_pred = model.predict(input_data).squeeze()
+                # 어떠한 인덱스 인지 뽑아낸다
+                i_pred = int(np.argmax(y_pred))
+                conf = y_pred[i_pred]
+                # confidence 가 90%이하이면 액션을 취하지 않았다 판단
+                if conf < 0.9:
+                    continue
+
+                action = actions[i_pred]
+                action_seq.append(action)
+
+                if len(action_seq) < 7:
+                    continue
+
+                # 마지막 3번 반복되었을 때 진짜로 판단
+                #this_action = '?'
+                if action_seq[-1] == action_seq[-2] == action_seq[-3]:
+                    #this_action = action
+
+                    if action == '삭제':
+                        word.clear()
+                    else:
+                        word.append(action)
+                img = Image.fromarray(img)
+                draw = ImageDraw.Draw(img)
+
+                draw.text(xy=(int(res.landmark[0].x*640), int(res.landmark[0].y*480 + 20)), text=action, font = font, fill=(255,255,255))
+                img = np.array(img)
+        content = ''
+        for i in word:
+            if i in content:
+                pass
+            else:
+                content += i
+                content += " "
+        img = Image.fromarray(img)
+        draw = ImageDraw.Draw(img)
+        draw.text(xy=(10,30), text=content, font = font, fill=(255,255,255))
         img = np.array(img)
 
         _, jpeg = cv2.imencode('.jpg', img)
