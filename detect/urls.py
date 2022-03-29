aba3fbab89052345de35d8007e43f6ebc2b2d653
hyunmin <choihm9903@naver.com>
2022-03-29 20:16:13 +0900

    feat: sign language

---
+++ detect/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import viewsdl
+from . import viewsml
+
+
+
+urlpatterns = [
+    path('detectML', viewsml.detectML, name='detectML'),
+    path('detectDL', viewsdl.detectDL, name='detectDL'),
+    path('', viewsml.home, name='home'),
+    path('ML/', viewsml.ML, name='ML'),    
+    path('DL/', viewsdl.DL, name='DL'),
+         
+]
