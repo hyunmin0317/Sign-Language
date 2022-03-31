aba4909eb65ca27107cf783b21bad1f1b9636bf3
hyunmin <choihm9903@naver.com>
2022-03-31 11:00:10 +0900

    feat: machine-learning

---
+++ ml/ml.py
@@ -0,0 +1,119 @@
+import time
+
+import cv2
+import mediapipe as mp
+import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+sum = []
+font = ImageFont.truetype("SCDream6.otf", 25)
+gesture = {
+    0: "자세히", 1: "보아야", 2: "예쁘다", 3: "오래", 4: "사랑스럽다", 5: "너도", 6: "그렇다", 7: "지우기"
+}
+gesture_en = {
+    0: "detail", 1: "see", 2: "beautiful", 3: "long", 4: "lovely", 5: "you", 6: "sodo", 7: "del"
+}
+
+startTime = time.time()
+sentence = ''
+file = np.genfromtxt('worddata.csv', delimiter=',')
+angle = file[:, :-1].astype(np.float32)
+label = file[:, -1].astype(np.float32)
+knn = cv2.ml.KNearest_create()  ## K-NN 알고리즘 객체 생성
+knn.train(angle, cv2.ml.ROW_SAMPLE, label)  ## train, 행 단위 샘플
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    max_num_hands=2,
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        joint = np.zeros((21, 3))  # 21개의 마디 부분 좌표 (x, y, z)를 joint에 저장
+        for j, lm in enumerate(hand_landmarks.landmark):
+            joint[j] = [lm.x, lm.y, lm.z]
+        # 벡터 계산
+        v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19], :]
+        v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], :]
+        v = v2 - v1
+
+        # 벡터 길이 계산 (Normalize v)
+        v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+        # arcos을 이용하여 15개의 angle 구하기
+        angle = np.arccos(np.einsum('nt,nt->n',
+                                    v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
+                                    v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :]))
+
+        angle = np.degrees(angle)  # radian 값을 degree로 변경
+
+        data = np.array([angle], dtype=np.float32)
+
+        ret, results, neighbours, dist = knn.findNearest(data, 3)
+        index = int(results[0][0])
+
+        if index in gesture.keys():
+            if time.time() - startTime > 3:
+                startTime = time.time()
+                # 다 지우기
+                if index == 7:
+                    sum.clear()
+                elif index == 8:
+                    # sentence = ''
+                    # #sum.append(gesture[index])
+                    sum.clear()
+                else:
+                    sum.append(gesture[index])  # 인식된 단어 리스트에 추가..
+                startTime = time.time()
+
+            cv2.putText(image, gesture_en[index].upper(), (
+                int(hand_landmarks.landmark[0].x * image.shape[1] - 10), int(hand_landmarks.landmark[0].y * image.shape[0] + 40)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
+
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+
+        image = Image.fromarray(image)
+        draw = ImageDraw.Draw(image)
+        for i in sum:
+            if i in sentence:
+                pass
+            else:
+                sentence += " "
+                sentence += i
+                print(sentence)
+
+        draw.text(xy=(20, 440), text=sentence, font=font, fill=(255, 255, 255))
+
+        image = np.array(image)
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
--- ml/viewsml.py
+++ ml/viewsml.py
@@ -4,38 +4,51 @@ from django.http import StreamingHttpResponse
 import cv2
 import mediapipe as mp
 import threading
-from PIL import ImageFont, ImageDraw, Image            ## 설치  ( pip install pillow )
+from PIL import ImageFont, ImageDraw, Image  ## 설치  ( pip install pillow )
+from hangul_utils import split_syllables, join_jamos
 import numpy as np
 import time
 
-
 sum = []
 
-font = ImageFont.truetype("SCDream6.otf", 25)
-        
+font = ImageFont.truetype("./static/fonts/SCDream6.otf", 25)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 gesture = {
-    0:"자세히",1:"보아야",2:"예쁘다",3:"오래",4:"사랑스럽다",5:"너도",6:"그렇다",7:"지우기"
+    0: "자세히", 1: "보아야", 2: "예쁘다", 3: "오래", 4: "사랑스럽다", 5: "너도", 6: "그렇다", 7: "지우기"
 }
 gesture_en = {
-    0:"detail", 1:"see", 2:"beautiful",3:"long",4:"lovely",5:"you",6:"sodo",7:"del"
+    0: "detail", 1: "see", 2: "beautiful", 3: "long", 4: "lovely", 5: "you", 6: "sodo", 7: "del"
 }
 
 ## 최대 1개의 손만 인식
 max_num_hands = 2
-hands = mp_hands.Hands(max_num_hands = max_num_hands,
-                    min_detection_confidence = 0.7,
-                    min_tracking_confidence = 0.7)
+hands = mp_hands.Hands(max_num_hands=max_num_hands,
+                       min_detection_confidence=0.7,
+                       min_tracking_confidence=0.7)
 
-file = np.genfromtxt('worddata.csv', delimiter=',')
-angle = file[:,:-1].astype(np.float32)
+file = np.genfromtxt('./data/worddata.csv', delimiter=',')
+angle = file[:, :-1].astype(np.float32)
 label = file[:, -1].astype(np.float32)
 
-knn = cv2.ml.KNearest_create()              ## K-NN 알고리즘 객체 생성
+knn = cv2.ml.KNearest_create()  ## K-NN 알고리즘 객체 생성
 knn.train(angle, cv2.ml.ROW_SAMPLE, label)  ## train, 행 단위 샘플
+
+
+# https://blog.miguelgrinberg.com/post/video-streaming-with-flask/page/8
+def home(request):
+    context = {}
+
+    return render(request, "home.html", context)
+
+
+def ML(request):
+    context = {}
+
+    return render(request, "ML.html", context)
+
 
 class VideoCamera(object):
     def __init__(self):
@@ -46,15 +59,14 @@ class VideoCamera(object):
 
     def __del__(self):
         self.video.release()
-    
 
-    def get_frame(self):        
-       
+    def get_frame(self):
+
         flag = []
-        
+
         prev_index = 0
         sentence = ''
-        recognizeDelay = 0.1    
+        recognizeDelay = 0.1
 
         image = self.frame
         video = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -72,14 +84,14 @@ class VideoCamera(object):
         #         joint = np.zeros((21, 3))
         #         for j,lm in enumerate(res.landmark):
         #             joint[j] = [lm.x, lm.y, lm.z]
-        if result.multi_hand_landmarks is not None:      # 손 인식 했을 경우
+        if result.multi_hand_landmarks is not None:  # 손 인식 했을 경우
             for res in result.multi_hand_landmarks:
-                joint = np.zeros((21, 3))                 # 21개의 마디 부분 좌표 (x, y, z)를 joint에 저장
-                for j,lm in enumerate(res.landmark):
+                joint = np.zeros((21, 3))  # 21개의 마디 부분 좌표 (x, y, z)를 joint에 저장
+                for j, lm in enumerate(res.landmark):
                     joint[j] = [lm.x, lm.y, lm.z]
                 # 벡터 계산
-                v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19],:]
-                v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],:]
+                v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19], :]
+                v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], :]
                 v = v2 - v1
 
                 # 벡터 길이 계산 (Normalize v)
@@ -87,20 +99,20 @@ class VideoCamera(object):
 
                 # arcos을 이용하여 15개의 angle 구하기
                 angle = np.arccos(np.einsum('nt,nt->n',
-                                            v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18],:],
-                                            v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19],:]))
+                                            v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
+                                            v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :]))
 
                 angle = np.degrees(angle)  # radian 값을 degree로 변경
 
                 data = np.array([angle], dtype=np.float32)
 
-                ret, results,neighbours,dist = knn.findNearest(data,3)
+                ret, results, neighbours, dist = knn.findNearest(data, 3)
                 index = int(results[0][0])
 
                 if index in gesture.keys():
                     if time.time() - self.startTime > 3:
                         self.startTime = time.time()
-                                # 다 지우기
+                        # 다 지우기
                         if index == 7:
                             sum.clear()
                         elif index == 8:
@@ -108,12 +120,14 @@ class VideoCamera(object):
                             # #sum.append(gesture[index])
                             sum.clear()
                         else:
-                            sum.append(gesture[index]) #인식된 단어 리스트에 추가..
+                            sum.append(gesture[index])  # 인식된 단어 리스트에 추가..
                         startTime = time.time()
 
-                    cv2.putText(video,gesture_en[index].upper(),(int(res.landmark[0].x * video.shape[1] - 10),int(res.landmark[0].y * video.shape[0] + 40)),cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),3)
+                    cv2.putText(video, gesture_en[index].upper(), (
+                    int(res.landmark[0].x * video.shape[1] - 10), int(res.landmark[0].y * video.shape[0] + 40)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
 
-                mp_drawing.draw_landmarks(video,res,mp_hands.HAND_CONNECTIONS)
+                mp_drawing.draw_landmarks(video, res, mp_hands.HAND_CONNECTIONS)
 
         video = Image.fromarray(video)
         draw = ImageDraw.Draw(video)
@@ -124,10 +138,10 @@ class VideoCamera(object):
                 sentence += " "
                 sentence += i
 
-        draw.text(xy=(20, 440), text = sentence, font=font, fill=(255, 255, 255))
-        
+        draw.text(xy=(20, 440), text=sentence, font=font, fill=(255, 255, 255))
+
         video = np.array(video)
-        
+
         _, jpeg = cv2.imencode('.jpg', video)
         return jpeg.tobytes()
 
@@ -135,11 +149,19 @@ class VideoCamera(object):
         while True:
             (self.grabbed, self.frame) = self.video.read()
 
+
 def gen(camera):
     while True:
         frame = camera.get_frame()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-cam = VideoCamera()
+@gzip.gzip_page
+def detectML(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  # This is bad! replace it with proper handling
+        print("에러입니다...")
+        pass
