f10458c72fed14140e15577abddcb63a4cc9678c
hyunmin <choihm9903@naver.com>
2022-04-06 15:11:54 +0900

    feat: model

--- project/alpha/SVM/model.py
+++ project/alpha/SVM/model.py
@@ -17,22 +17,26 @@ x = data[col[:-1]]
 print(x)
 y = data[col[-1]]
 
+
 #PipeLine
 scaler = StandardScaler()
 base_model = SVC(kernel='rbf')
 pipe = Pipeline([('scaler', scaler), ('base_model', base_model)])
 
 
-
 # dataset split
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
 
 # grid search을 통한 svm 파라미터 결정시 필요한 요소 설정
 parameters = {'base_model__C': [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000],
           'base_model__gamma': [0.0001, 0.001, 0.01, 1, 10, 100, 1000]}
+
+
 # grid search Part
 grid = GridSearchCV(estimator=pipe, param_grid=parameters, cv=5, n_jobs=-1)
 grid.fit(X_train, y_train)
+
 
 # grid search 결과 출력
 print(grid)
@@ -41,7 +45,6 @@ print(grid.best_estimator_)
 print(grid.predict(X_test))
 print("학습 결과", grid.score(X_train, y_train))
 print("테스트 결과", grid.score(X_test, y_test))
-
 
 
 #시각화
